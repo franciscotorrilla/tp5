5402a2caa28b7511f2e3b5856d68de364da9c59b
nres98 <43898082+nres98@users.noreply.github.com>
2018-12-02 23:12:14 -0300

    Add files via upload

--- Aeropuerto.h
+++ Aeropuerto.h
@@ -22,16 +22,51 @@ class Aeropuerto {
 
     //metodos
 	public:
+	    //constructor
+	    //pre: recibe el codigo IATA y los datos del aeropuerto
+	    //post: crea el aeropuerto
         Aeropuerto(string codigo, dato* datos);
+
+        //pre: recibe un codigo IATA
+        //pos: le asigna el codigo
         void asignarCodigo(string codigo);
+
+
+        //pre: recibe los datos del aeropuerto
+        //pos: le asigna los datos al aeropuerto
         void asignarDatos(dato* datos);
+
+        //pre: recibe un puntero a un vuelo
+        //pos: le asigna ese vuelo como primer vuelo del aeropuerto
         void asignarPrimerVuelo(Vuelo* primerVuelo);
+
+        //pre: recibe un puntero a un aeropuerto
+        //pos: le asigna al aeropuerto su suiguiente aeropuerto
         void asignarSiguienteAeropuerto(Aeropuerto* siguienteAeropuerto);
+
+        //pre:-
+        //post: devuelve el codigo IATA del aeropuerto
         string obtenerCodigo();
+
+        //pre: -
+        //post: devuelve un puntero a los datos del aeropuerto
         dato* obtenerDatos();
+
+        //pre: -
+        //post: devuelve un puntero al primer vuelo del aeropuerto
         Vuelo* obtenerPrimerVuelo();
+
+        //pre: -
+        //post: devuelve un puntero al siguiente aeropuerto
         Aeropuerto* obtenerSiguienteAeropuerto();
+
+        //pre: recibe un puntero a un vuelo
+        //post: le asigna el vuelo despues del ultimo vuelo que tenia el aeropuerto
         void asignarVuelo(Vuelo* vuelo);
+
+        //destructor
+        //pre: -
+        //pos: borra los datos del aeropuerto
         ~Aeropuerto();
 };
 
--- Vuelo.h
+++ Vuelo.h
@@ -12,10 +12,25 @@ class Vuelo {
 
     //metodos
 	public:
+	    //constructor
+	    //pre: recibe el precio y un puntero al aeropuerto de llegada
+	    //pos: se crea el vuelo
         Vuelo(unsigned precio, Aeropuerto* aeropuertoLlegada);
+
+        //pre: recibe un puntero al siguiente vuelo
+        //pos: coloca el vuelo recibido como siguiente vuelo
         void asignarSiguienteVuelo(Vuelo* siguienteVuelo);
+
+        //pre: -
+        //pos: devuelve el precio del vuelo
         unsigned obtenerPrecio();
+
+        //pre: -
+        //pos: devuelve un puntero al aeropuerto de llegada
         Aeropuerto* obtenerAeropuertoLlegada();
+
+        //pre: -
+        //pos: devuelve un puntero al siguiente vuelo
         Vuelo* obtenerSiguienteVuelo();
 };
 
